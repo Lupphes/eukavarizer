5dc926d23f9b8150de735734e097cc16e5fa66a8
Lupphes <dev@lupphes.com>
2025-03-30 22:21:16 +0200

    Add resources to the scripts

--- kube/eukavarizer_job_nano.sh
+++ kube/eukavarizer_job_nano.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N eukavarizer_job_nano
-#PBS -l select=1:ncpus=16:mem=256gb:scratch_local=300gb
+#PBS -l select=1:ncpus=8:mem=512gb:scratch_local=800gb
 #PBS -l walltime=24:00:00
 #PBS -m abe
 #PBS -M ondrej.sloup@protonmail.com
