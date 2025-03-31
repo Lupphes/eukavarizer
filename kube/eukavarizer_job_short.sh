98038b1229b9a3660e01cc4cdf8e9f78be5f74ef
Lupphes <dev@lupphes.com>
2025-03-31 21:17:11 +0200

    Update configs

--- kube/eukavarizer_job_short.sh
+++ kube/eukavarizer_job_short.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N eukavarizer_job_short
-#PBS -l select=1:ncpus=64:mem=1500gb:scratch_local=3450gb
+#PBS -l select=1:ncpus=64:mem=1500gb:scratch_local=3000gb
 #PBS -l walltime=24:00:00
 #PBS -m abe
 #PBS -M ondrej.sloup@protonmail.com
