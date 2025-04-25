caca0d9494de63ec067ea8b28e33cb3352e235a4
Lupphes <dev@lupphes.com>
2025-04-25 20:58:02 +0200

    Add memory

--- kube/metacentrum.sh
+++ kube/metacentrum.sh
@@ -19,6 +19,8 @@ export NXF_WORK=/storage/brno2/home/luppo/work
 export NXF_LOG_FILE=/storage/brno2/home/luppo/logs/.nextflow.log
 
 cd eukavarizer
+chmod +x bin/svaba_annotate.py
+chmod +x bin/simple-event-annotation.R
 ./../nextflow run main.nf -profile mamba,mix_medium,qc_off -resume
 ./../nextflow run main.nf -profile mamba,mix_medium,qc_off --taxonomy_id 9606 --reference_genome "/storage/brno2/home/luppo/data/9606/ref/hg38.fa.gz" --sequence_dir "/storage/brno2/home/luppo/data/9606/gib" --outdir /storage/brno2/home/luppo/out -resume
 
