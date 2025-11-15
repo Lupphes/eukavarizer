d2891fc16304a30e8b741dc081cb56ba221f4d96
Ondrej Sloup <dev@lupphes.com>
2025-11-15 22:07:22 +0100

    Lower the req

--- run_scripts/eukavarizer_job_short.sh
+++ run_scripts/eukavarizer_job_short.sh
@@ -3,7 +3,7 @@
 # PBS Job: Eukavarizer Pipeline - Homo sapiens (Human, Taxonomy 9606) - Short Reads
 #========================================================================================
 #PBS -N eukavarizer_human_9606_short
-#PBS -l select=1:ncpus=64:mem=1500gb:scratch_local=3000gb
+#PBS -l select=1:ncpus=32:mem=728gb:scratch_local=3000gb
 #PBS -l walltime=48:00:00
 #PBS -m abe
 #PBS -M ondrej.sloup@protonmail.com
